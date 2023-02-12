3f3a81d64a54d19cdfa92b90b6e52ad64c98c5fb
Blabzillaweasel <connormcgeek@gmail.com>
2023-02-12 19:50:20 +1300

    Made the plugin network compatible to ensure that all players get the same result.
    I discovered in local testing that the callback from the videoplayer seemed to only happen to the local player. So I changed the script to send a network event to all players announcing which audio input Audiolink should be using.

--- Assets/Audiolink (USharpVideo Adapter)/Scripts/AL_USV_Adapter.cs
+++ Assets/Audiolink (USharpVideo Adapter)/Scripts/AL_USV_Adapter.cs
@@ -1,17 +1,27 @@
-﻿using UdonSharp;
+﻿
+using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
 using UdonSharp.Video;
 
-public class AL_USV_Adapter: UdonSharpBehaviour
+[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+public class AL_USV_Adapter : UdonSharpBehaviour
 {
 	//Points to the USVPlayer so that Stream mode can be checked
     public USharpVideoPlayer videoPlayer;
+	
+	//Connects to the Audiolink component
 	public VRCAudioLink.AudioLink audioLink;
+	
+	//Connects to the Video audio source
 	public AudioSource videoAudioSource;
+	
+	//Connects to the Stream audio source
 	public AudioSource streamAudioSource;
 	
+	//Declares a synced variable, used to check the audio settings and set them if necessary.
+	[UdonSynced] private bool isStreamMode;
 	
 	//Registers to the USharpVideoPlayers events
 	public void Start()
@@ -24,7 +34,6 @@ public class AL_USV_Adapter: UdonSharpBehaviour
 	{
 		CheckVidPlayerMode();
 	}
-	
 	//Checks if the USVPlayer is in Stream mode, and adjusts the Audiolink audiosource if necessary
 	public void CheckVidPlayerMode()
 	{
@@ -34,38 +43,64 @@ public class AL_USV_Adapter: UdonSharpBehaviour
 			//Announces the state of stream mode
 			Debug.Log("[<color=#ff00c6>USV_Audiolink_Adapter</color>]" + "Video player is in stream mode");
 			
-			//Checks if the Audiolink source is set to the appropriate setting and changes it if not set correctly.
-			if (audioLink.audioSource == streamAudioSource)
-			{
-				Debug.Log("[<color=#ff00c6>USV_Audiolink_Adapter</color>]" + "No action necessary, the player is in stream mode and Audiolink is already set to " + audioLink.audioSource.name);
-				return;
-			}
-				
-				else
-			{
-				audioLink.audioSource = streamAudioSource;
-				Debug.Log("[<color=#ff00c6>USV_Audiolink_Adapter</color>]" + "Audio Source set to " + audioLink.audioSource.name);
-			}
+			//Sends a network event informing players what state the player is in
+			SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All,"StreamModeActive");
         }
-		
 		else
 		{
-			//Announces that the player is not in stream mode
+			//Announces that the player is NOT in stream mode
 			Debug.Log("[<color=#ff00c6>USV_Audiolink_Adapter</color>]" + "Video player is NOT in stream mode");
+		
+			//Sets streamMode bool to false
+			isStreamMode = false;
 			
-			//Checks if the Audiolink source is set to the appropriate setting and changes it if not set correctly.
-			if (audioLink.audioSource == videoAudioSource)
-			{
-				Debug.Log("[<color=#ff00c6>USV_Audiolink_Adapter</color>]" + "No action necessary, the player is in video mode and Audiolink is already set to " + audioLink.audioSource.name);
-				return;;
-				}
-			
-			else
-			{
-				audioLink.audioSource = videoAudioSource;
-				Debug.Log("[<color=#ff00c6>USV_Audiolink_Adapter</color>]" + "Audio Source set to " + audioLink.audioSource.name);
-			}
+			//Sends a network event informing players what state the player is in
+			SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All,"VideoModeActive");
 		}
-    }
-}
+	}
+	public void StreamModeActive()
+	{
+		//Sets streamMode bool to true
+		isStreamMode = true;
+		
+		//Checks if the Audiolink source is set to the appropriate setting and changes it if not set correctly.
+		if (audioLink.audioSource == streamAudioSource)
+		{
+			Debug.Log("[<color=#ff00c6>USV_Audiolink_Adapter</color>]" + "No action necessary, the player is in video mode and Audiolink is already set to " + audioLink.audioSource.name);
+			return;;
+		}
+		else
+		{
+			audioLink.audioSource = streamAudioSource;
+			Debug.Log("[<color=#ff00c6>USV_Audiolink_Adapter</color>]" + "Audio Source set to " + audioLink.audioSource.name);
+		}
+	}
+	public void VideoModeActive()
+	{
+		//Sets streamMode bool to false
+		isStreamMode = false;
 
+		//Checks if the Audiolink source is set to the appropriate setting and changes it if not set correctly.
+		if (audioLink.audioSource == videoAudioSource)
+		{
+			Debug.Log("[<color=#ff00c6>USV_Audiolink_Adapter</color>]" + "No action necessary, the player is in video mode and Audiolink is already set to " + audioLink.audioSource.name);
+			return;;
+		}
+		else
+		{
+			audioLink.audioSource = videoAudioSource;
+			Debug.Log("[<color=#ff00c6>USV_Audiolink_Adapter</color>]" + "Audio Source set to " + audioLink.audioSource.name);
+		}
+	}
+	public override void OnDeserialization()
+	{
+		if (isStreamMode == true)
+		{
+			StreamModeActive();
+		}
+		else
+		{
+			VideoModeActive();
+		}
+	}
+}
